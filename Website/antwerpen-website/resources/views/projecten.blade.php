5be8078fcac76ce995d9e5230f57a8761ecf3e3d
DriesH <dries.heyninck@gmail.com>
2016-04-22 15:03:56 +0200

    redo van commit

--- Website/antwerpen-website/resources/views/projecten.blade.php
+++ Website/antwerpen-website/resources/views/projecten.blade.php
@@ -3,15 +3,7 @@
 
 @section('content')
 
-<div>
-    <p>testlalalala</p>
-</div>
-
-<<<<<<< HEAD
 <div class="grid">
-    <div class="grid-sizer"></div>
-    <div class="gutter-sizer"></div>
-
     @foreach ($projecten as $project)
 
         <div class="project-box">
@@ -33,26 +25,3 @@
 </div>
 
 @endsection
-=======
-<ul id="project-container">
-@foreach ($projecten as $project)
-
-    <li class="titles-wrap animated project-box">
-       <img src=" {{$project->foto}}" alt="">
-        <article>
-            <time> {{ $project->created_at }} </time>
-            <h1>
-                <a href="">{{ $project->naam }}</a>
-            </h1>
-            <p>{{ str_limit($project->uitleg, $limit = 250, $end='...')  }}</p>
-            <div>
-                <a href="">meer lezen</a>
-            </div>
-        </article>
-    </li>
-            
-@endforeach
-</ul>
-
-@endsection
->>>>>>> refs/remotes/origin/master
