1394342008685b63a4c5920dc1f5060175971d1c
DriesH <dries.heyninck@gmail.com>
2016-05-02 20:16:25 +0200

    responsive frontpage design

--- Website/antwerpen-website/resources/views/projecten.blade.php
+++ Website/antwerpen-website/resources/views/projecten.blade.php
@@ -32,31 +32,31 @@
 </div>-->
 
 <div class="grid">
-    @foreach ($projecten as $project)
+    <div class="row">
+        @foreach ($projecten as $project)
+        <div class="col-sm-6 col-md-4 project-box">
+            <div class="thumbnail">
+               <a href="project/{{$project->idProject}}"><img src="{{$project->foto}}" alt=""></a>
+                <article>
+                    <time> {{ $project->created_at }} </time>
+                    <h5>
+                        <a href="project/{{$project->idProject}}">{{ $project->naam }}</a>
+                    </h5>
+                    <p>{{ str_limit($project->uitleg, $limit = 250, $end='...')  }}</p>
+                    <div>
+                        <a class="meerlezen" href="project/{{$project->idProject}}"> meer lezen</a>
+                    </div>
+                </article>
+                <div class="project-box-footer">
+                    @if (!Auth::guest() && Auth::user()->role == 10)
+                        <a href="admin/project-bewerken/{{$project->idProject}}" class="bewerken-link">Bewerken</a>
+                    @endif
 
-        <div class="project-box">
-           <a href="project/{{$project->idProject}}"><img src="{{$project->foto}}" alt=""></a>
-            <article>
-                <time> {{ $project->created_at }} </time>
-                <h5>
-                    <a href="project/{{$project->idProject}}">{{ $project->naam }}</a>
-                </h5>
-                <p>{{ str_limit($project->uitleg, $limit = 250, $end='...')  }}</p>
-                <div>
-                    <a class="meerlezen" href="project/{{$project->idProject}}"> meer lezen</a>
+                    <a href="#" class="footer-link">Test Categorie</a>
                 </div>
-            </article>
-            <div class="project-box-footer">
-                @if (!Auth::guest() && Auth::user()->role == 10)
-                    <a href="admin/project-bewerken/{{$project->idProject}}" class="bewerken-link">Bewerken</a>
-                @endif
-                
-                <a href="#" class="footer-link">Test Categorie</a>
             </div>
         </div>
-
-    @endforeach
-
+        @endforeach
+    </div>
 </div>
-
 @endsection
