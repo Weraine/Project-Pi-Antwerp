cb100a9184ac1120baef04f624a55e99ffb4c678
Dries Heyninck <dries.heyninck@gmail.com>
2016-05-03 18:47:02 +0200

    fonts added

--- Website/antwerpen-website/resources/views/admin/admin-panel.blade.php
+++ Website/antwerpen-website/resources/views/admin/admin-panel.blade.php
@@ -11,17 +11,17 @@
         <div class="row">
             <div class="col-md-4">
                 <div>
-                    <a class="btn btn-success" href="/admin/nieuwproject"><i class="fa fa-plus"></i>Nieuw project toevoegen</a>
+                    <a class="btn btn-success center-block" href="/admin/nieuwproject"><i class="fa fa-plus"></i>Nieuw project toevoegen</a>
                 </div>
             </div>
             <div class="col-md-4">
                 <div>
-                    <a class="btn btn-warning" href="/admin/nieuwproject"><i class="fa fa-pencil-square-o"></i>Project wijzigen</a>
+                    <a class="btn btn-warning center-block" href="/admin/nieuwproject"><i class="fa fa-pencil-square-o"></i>Project wijzigen</a>
                 </div>
             </div>
             <div class="col-md-4">
                 <div>
-                    <a class="btn btn-danger" href="/admin/nieuwproject"><i class="fa fa-trash-o"></i>Project verwijden</a>
+                    <a class="btn btn-danger center-block" href="/admin/nieuwproject"><i class="fa fa-trash-o"></i>Project verwijden</a>
                 </div>
             </div>
         </div>
--- Website/antwerpen-website/resources/views/layouts/app.blade.php
+++ Website/antwerpen-website/resources/views/layouts/app.blade.php
@@ -11,6 +11,9 @@
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
         <link rel="stylesheet" type="text/css" href="http://cdn.jsdelivr.net/jquery.slick/1.5.9/slick.css"/>
         <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" type="text/css">
+        <!-- FONTS -->
+        <link rel="stylesheet" href="/fonts/Antwerpen-Regular.css" media="screen" title="no title" charset="utf-8">
+        <link rel="stylesheet" href="/fonts/Sun-Antwerpen.css" media="screen" title="no title" charset="utf-8">
         <!-- PAGE LAYOUT -->
         <link rel="stylesheet" href="/css/master.css" media="screen" title="no title" charset="utf-8">
         <link rel="stylesheet" href="/css/nav-bar.css" media="screen" title="no title" charset="utf-8">
