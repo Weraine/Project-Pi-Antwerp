5f0df177e5ac387465ad5bf0ab1a88be7575e1a6
Dries Heyninck <dries.heyninck@gmail.com>
2016-05-04 18:13:56 +0200

    dashboard styling

--- Website/antwerpen-website/resources/views/layouts/app.blade.php
+++ Website/antwerpen-website/resources/views/layouts/app.blade.php
@@ -21,6 +21,7 @@
         <link rel="stylesheet" href="/css/project-page.css" media="screen" title="no title" charset="utf-8">
         <link rel="stylesheet" href="/css/carousel.css" media="screen" title="no title" charset="utf-8">
         <link rel="stylesheet" href="/css/admin-panel.css" media="screen" title="no title" charset="utf-8">
+        <link rel="stylesheet" href="/css/dashboard-user.css" media="screen" title="no title" charset="utf-8">
         <!-- FUNCTIONAL CSS -->
         <link rel="stylesheet" href="/css/vertical-timeline-css.css" media="screen" title="no title" charset="utf-8">
         <link rel="stylesheet" href="/css/slick-theme.css" media="screen" title="no title" charset="utf-8">
@@ -67,7 +68,7 @@
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
         <script type="text/javascript" src="https://cdn.jsdelivr.net/jquery.slick/1.5.9/slick.min.js"></script>
-        <script src="/js/masonry.min.js" charset="utf-8"></script>
+        <script src="/js/salvatorre.min.js" charset="utf-8"></script>
         <script src="/js/modernizr.js" charset="utf-8"></script>
         <script src="/js/main.js" charset="utf-8"></script>
 
